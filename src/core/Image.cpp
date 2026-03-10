24d756c49ff4b49c830612461fb557724b9d61b7
numname869 <izakrzyzowska0@gmail.com>
2026-03-10 21:30:16 +0100

    First objects, likely to add more structs soon

--- src/core/Image.cpp
+++ src/core/Image.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Image.h"
+#include <vector>
+
+
+public class Image  {
+
+private:
+    std::vector<Layer> layers;
+    size_t width, height;
+
+public:
+    void addLayer() {
+        layers.emplace_back();
+    }
+
+    void removeLayer(size_t index) {
+        if (index < layers.size()) {
+            layers.erase(layers.begin() + index);
+        }
+    }
+
+    Layer& getLayer(size_t index) {
+        return layers.at(index);
+    }
+
+    size_t layerCount() const { return layers.size(); }
+};
+
+
