5720813248769f38cf1901576bdc43fa71f8041f
Wolfress <Wolfress3@gmail.com>
2026-04-19 21:56:09 +0200

    adaptery kształtów prostokąt i koło

--- src/Tools/ITools.cpp
+++ src/Tools/ITools.cpp
@@ -28,3 +28,101 @@ void Eraser::execute(Document& doc, int x, int y, int PixelSize, Color drawcolor
 const char* Eraser::getName() const {
     return "Eraser";
 }
+class RectangleTool : public ITool {
+private:
+    bool active = false;
+    int sx = 0, sy = 0;
+
+public:
+    const char* getName() const override { return "Rectangle"; }
+
+    void execute(Document& doc,
+        ToolEvent event,
+        int x,
+        int y,
+        int PixelSize,
+        Color color) override
+    {
+        if (event == ToolEvent::Down) {
+            active = true;
+            sx = x;
+            sy = y;
+            return;
+        }
+
+        if (event == ToolEvent::Up && active) {
+
+            int minX = std::min(sx, x);
+            int maxX = std::max(sx, x);
+            int minY = std::min(sy, y);
+            int maxY = std::max(sy, y);
+
+            // top + bottom
+            for (int i = minX; i <= maxX; i += PixelSize) {
+                doc.setPixel(i, minY, color, PixelSize);
+                doc.setPixel(i, maxY, color, PixelSize);
+            }
+
+            // left+ right
+            for (int j = minY; j <= maxY; j += PixelSize) {
+                doc.setPixel(minX, j, color, PixelSize);
+                doc.setPixel(maxX, j, color, PixelSize);
+            }
+
+            active = false;
+        }
+    }
+};
+class CircleTool : public ITool {
+private:
+    bool active = false;
+    int cx = 0, cy = 0;
+
+public:
+    const char* getName() const override { return "Circle"; }
+
+    void execute(Document& doc,
+        ToolEvent event,
+        int x,
+        int y,
+        int PixelSize,
+        Color color) override
+    {
+        if (event == ToolEvent::Down) {
+            active = true;
+            cx = x;
+            cy = y;
+            return;
+        }
+
+        if (event == ToolEvent::Up && active) {
+
+            int dx = x - cx;
+            int dy = y - cy;
+            int r = (int)sqrt(dx * dx + dy * dy);
+
+            int px = r;
+            int py = 0;
+            int err = 0;
+
+            while (px >= py) {
+                doc.setPixel(cx + px, cy + py, color, PixelSize);
+                doc.setPixel(cx + py, cy + px, color, PixelSize);
+                doc.setPixel(cx - py, cy + px, color, PixelSize);
+                doc.setPixel(cx - px, cy + py, color, PixelSize);
+                doc.setPixel(cx - px, cy - py, color, PixelSize);
+                doc.setPixel(cx - py, cy - px, color, PixelSize);
+                doc.setPixel(cx + py, cy - px, color, PixelSize);
+                doc.setPixel(cx + px, cy - py, color, PixelSize);
+
+                py++;
+                if (err <= 0) err += 2 * py + 1;
+                if (err > 0) { px--; err -= 2 * px + 1; }
+            }
+
+            active = false;
+        }
+    }
+};
+
+
