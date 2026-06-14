65c1cdb14ea360289786b3ee98d55f234a9fcea2
numname869 <izakrzyzowska0@gmail.com>
2026-06-14 17:52:10 +0200

    Edited Header File, Saving, Loading

--- src/file/file.h
+++ src/file/file.h
@@ -4,22 +4,30 @@
 
 #ifndef PIXELEDITOR_FILE_H
 #define PIXELEDITOR_FILE_H
-#include <cstdint>
-#include "..\core\Types.h"
-#include <vector>
+
+
 #pragma once
-#endif //PIXELEDITOR_FILE_H
+#include <vector>
+#include <string>
+#include "../core/structs.h"
 
-void WriteFileType(const char* filename, u32 h, u32 w, const std::vector<Color>& image);
-std::vector<Color> ReadFileType(const char* filename, uint32_t& out_w, uint32_t& out_h);
-std::string open_file_dialog_windows();
-
-struct ppe_header
-{
-#define PPE_MAGIC_VALUE_ PPE_CODE('p', 'p', 'e', 'f')
-    u32 MagicValue;
-    u32 h;
-    u32 w;
-    u32 PixelsPerLayer;
-    u64 PixelOffset;
+#pragma pack(push, 1)
+struct ppe_header {
+    uint32_t MagicValue;       // 0x505045 ("PPE")
+    uint32_t w;
+    uint32_t h;
+    uint32_t LayerCount;
 };
+
+struct ppe_layer_header {
+    uint32_t Id;
+    uint8_t visible;
+    uint32_t name_length;
+};
+#pragma pack(pop)
+
+void SaveDocumentPPE(const char* filename, const Document& doc);
+Document* LoadDocumentPPE(const char* filename);
+std::string open_file_dialog_windows();
+std::string save_file_dialog_windows();
+#endif //PIXELEDITOR_FILE_H
